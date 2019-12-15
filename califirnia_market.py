0f417ddc681a5aecb93dcec76f74925ed9487bbc
Natalie <donatalie33@gmail.com>
2019-12-14 21:13:15 -0800

    my

--- califirnia_market.py
+++ califirnia_market.py
@@ -3,26 +3,16 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
-driver.get("https://google.com")
+driver.get("https://svts10182803.wordpress.com/")
 #driver.maximize_window()
+driver.find_element(By.ID, 'g2-name').send_keys("name")
+driver.find_element(By.ID, 'g2-email').send_keys("1234@gmail.com")
+driver.find_element(By.ID, 'contact-form-comment-g2-message').send_keys("hello")
+driver.find_element(By.CLASS_NAME, 'pushbutton-wide').submit()
 
-driver.find_element(By.NAME, "q").send_keys("abc")
 
-print(driver.find_element(By.TAG_NAME, "img").get_attribute("src"))
-print(driver.find_element(By.NAME, "btnK").get_attribute("value"))
-print(driver.find_element(By.NAME, "btnI").get_attribute("value"))
 
-driver.find_element(By.NAME, "btnK").click()
-driver.find_element(By.PARTIAL_LINK_TEXT, "ABC Home Page").click()
-assert "ABC Home Page - ABC.com" in driver.title
 
 driver.implicitly_wait(10)
-driver.find_element(By.XPATH, "//span[@class='Searchlist__icon__search']").click()
-driver.find_element(By.XPATH, "//input[@placeholder='search for a show']").send_keys("Dancing")
-driver.implicitly_wait(10)
-# Find el Dancing
-driver.find_element(By.XPATH, "//a[@class='AnchorLink fitt-tracker']//img[@class='aspect-ratio--child']").click()
-driver.find_element(By.XPATH, "//a[@href='/shows/dancing-with-the-stars/episode-guide/season-28']").click()
-assert "Dancing with the Stars Full Episodes | Watch Online | ABC" in driver.title
 
 driver.close()
